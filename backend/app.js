b347018ba7846b98024cc0bb35e8ebce094d5e17
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2025-07-11 12:20:57 +0200

    http -> https

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const purchaseRoutes = require('./routes/purchase');
 const cookieParser = require('cookie-parser');
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://128.140.40.152:3000', 'https://tworczasiec.pl'],
+  origin: ['http://localhost:3000', 'http://128.140.40.152:3000', 'https://128.140.40.152:3001', 'https://tworczasiec.pl'],
   credentials: true
 }));
 
--- frontend/src/services/request.js
+++ frontend/src/services/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'http://128.140.40.152:3001/api',
+    baseURL: 'https://128.140.40.152:3001/api',
 });
 
 
