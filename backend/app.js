5a22da1071a53fa3caba82c20445484c1f5f01bd
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2025-07-11 10:44:35 +0200

    test changes

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use('/api/products', productsRoutes);
 app.use('/api/purchase', purchaseRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
